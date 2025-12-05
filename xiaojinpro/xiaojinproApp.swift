4756433da58ce3fd39705fae419ecac9ac1b21b8
rickyjin <jjrrqqq4235@yeah.net>
2025-12-05 13:48:00 +0800

    Initial Commit

---
+++ xiaojinpro/xiaojinproApp.swift
@@ -0,0 +1,17 @@
+//
+//  xiaojinproApp.swift
+//  xiaojinpro
+//
+//  Created by 靳晨 on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct xiaojinproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
